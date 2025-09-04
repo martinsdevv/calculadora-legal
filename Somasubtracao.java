6f170352f4c4d88b79c959a3baef81c59b08e6c1
sophiaeduardalima-ship-it <sophiaeduardalima@gmail.com>
2025-09-03 22:39:48 -0300

    versão soma e subtração

---
+++ Somasubtracao.java
@@ -0,0 +1,12 @@
+public class Somasubtracao {
+
+    public double somar(double a, double b) {
+        return a + b;
+    }
+
+    public double subtrair(double a, double b) {
+        return a - b;
+    }
+}
+
+
